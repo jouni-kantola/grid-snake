600859c6c3cf79e52f1e8679ebfc669206f1ac2a
Jouni Kantola <jouni@kantola.se>
2020-01-18 22:57:28 +0100

    Move snake up

--- grid-snake/index.js
+++ grid-snake/index.js
@@ -1,11 +1,16 @@
 const start = document.querySelector("#start");
 const grid = document.querySelector(".grid");
 
-let numberOfCells;
-
 const snakeHeadCssClass = "head";
 const snakeTailCssClass = "tail";
-let numberOfColumns, numberOfRows;
+let numberOfColumns, numberOfRows, numberOfCells;
+const directions = {
+  up: "ArrowUp",
+  right: "ArrowRight",
+  down: "ArrowDown",
+  left: "ArrowLeft"
+};
+let direction = directions.right;
 
 const game = () => {
   let headPos = Math.floor(Math.random() * numberOfCells);
@@ -17,11 +22,20 @@ const game = () => {
       cells[headPos].classList.remove(snakeHeadCssClass);
       cells[tailPos].classList.remove(snakeTailCssClass);
 
-      const row = Math.ceil(headPos / numberOfRows);
-      headPos = headPos + 1;
-      if (headPos / row >= numberOfColumns)
-        headPos = (row - 1) * numberOfColumns + 1;
-      tailPos = headPos - 1;
+      // TODO: When wrapping, should just wrap around head
+      if (direction === directions.right) {
+        const row = Math.ceil(headPos / numberOfRows);
+        headPos = headPos + 1;
+        if (headPos / row >= numberOfColumns)
+          headPos = (row - 1) * numberOfColumns + 1;
+        tailPos = headPos - 1;
+      } else if (direction === directions.up) {
+        headPos = headPos - numberOfColumns;
+        if (headPos <= 0)
+          headPos =
+            (numberOfRows - 1) * numberOfColumns + (headPos % numberOfColumns);
+        tailPos = headPos + numberOfColumns;
+      }
 
       cells[headPos].classList.add(snakeHeadCssClass);
       cells[tailPos].classList.add(snakeTailCssClass);
@@ -50,4 +64,21 @@ start.addEventListener("click", () => {
 
     game();
   });
+});
+
+window.addEventListener("keydown", event => {
+  switch (event.key) {
+    case "ArrowLeft":
+      direction = directions.left;
+      break;
+    case "ArrowUp":
+      direction = directions.up;
+      break;
+    case "ArrowRight":
+      direction = directions.right;
+      break;
+    case "ArrowDown":
+      direction = directions.down;
+      break;
+  }
 });
