951b9edcf8d88a216a21ca734ec5ee7361a1e8e9
Jouni Kantola <jouni@kantola.se>
2020-01-18 22:32:30 +0100

    Snake on the move

--- grid-snake/index.js
+++ grid-snake/index.js
@@ -1,11 +1,40 @@
 const start = document.querySelector("#start");
 const grid = document.querySelector(".grid");
 
-start.addEventListener("click", () => {
-  const numberOfColumns = +document.querySelector("#columns").value;
-  const numberOfRows = +document.querySelector("#rows").value;
+let numberOfCells;
 
-  const cells = new Array(numberOfColumns * numberOfRows)
+const snakeHeadCssClass = "head";
+const snakeTailCssClass = "tail";
+let numberOfColumns, numberOfRows;
+
+const game = () => {
+  let headPos = Math.floor(Math.random() * numberOfCells);
+  let tailPos = headPos - 1;
+  const cells = grid.querySelectorAll(".cell");
+
+  setInterval(() => {
+    requestAnimationFrame(() => {
+      cells[headPos].classList.remove(snakeHeadCssClass);
+      cells[tailPos].classList.remove(snakeTailCssClass);
+
+      const row = Math.ceil(headPos / numberOfRows);
+      headPos = headPos + 1;
+      if (headPos / row >= numberOfColumns)
+        headPos = (row - 1) * numberOfColumns + 1;
+      tailPos = headPos - 1;
+
+      cells[headPos].classList.add(snakeHeadCssClass);
+      cells[tailPos].classList.add(snakeTailCssClass);
+    });
+  }, 400);
+};
+
+start.addEventListener("click", () => {
+  numberOfColumns = +document.querySelector("#columns").value;
+  numberOfRows = +document.querySelector("#rows").value;
+
+  numberOfCells = numberOfColumns * numberOfRows;
+  const cells = new Array(numberOfCells)
     .fill(document.createElement("span"))
     .map(span => {
       const cell = span.cloneNode();
@@ -18,5 +47,7 @@ start.addEventListener("click", () => {
     cells.forEach(cell => {
       grid.appendChild(cell);
     });
+
+    game();
   });
 });
