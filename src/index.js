b5ce3eabd14cc8a1ca0ae90f3c0c7b6a094d9534
Jouni Kantola <jouni@kantola.se>
2021-04-29 23:46:43 +0200

    Format with prettier

--- src/index.js
+++ src/index.js
@@ -10,303 +10,299 @@ const snakeTailCssClass = "tail";
 const heartCssClass = "heart";
 
 const directions = {
-    up: "ArrowUp",
-    right: "ArrowRight",
-    down: "ArrowDown",
-    left: "ArrowLeft"
+  up: "ArrowUp",
+  right: "ArrowRight",
+  down: "ArrowDown",
+  left: "ArrowLeft",
 };
 
 const Snake = (numberOfColumns, numberOfRows) => {
-    return {
-        move: (from, direction) => {
-            switch (direction) {
-                case directions.left: {
-                    const updated = from - 1;
-                    const row = Math.ceil(updated / numberOfColumns);
-                    const wrapAround = from % numberOfColumns === 0;
-                    if (wrapAround && !useHip) {
-                        return false;
-                    }
-                    return wrapAround
-                        ? row * numberOfColumns + numberOfColumns - 1
-                        : updated;
-                }
-                case directions.up: {
-                    const updated = from - numberOfColumns;
-                    const wrapAround = updated < 0;
-                    if (wrapAround && !useHip) return false;
-                    return wrapAround
-                        ? numberOfRows * numberOfColumns +
-                              (from % numberOfColumns) -
-                              numberOfColumns
-                        : updated;
-                }
-                case directions.down: {
-                    const updated = from + numberOfColumns;
-                    const wrapAround =
-                        Math.ceil(updated / numberOfColumns) > numberOfRows ||
-                        updated >= numberOfColumns * numberOfRows;
-                    if (wrapAround && !useHip) return false;
-                    return wrapAround ? from % numberOfColumns : updated;
-                }
-                default: {
-                    let updated = from + 1;
-                    const wrapAround = updated % numberOfColumns === 0;
-                    if (wrapAround && !useHip) return false;
-                    return wrapAround ? updated - numberOfColumns : updated;
-                }
-            }
+  return {
+    move: (from, direction) => {
+      switch (direction) {
+        case directions.left: {
+          const updated = from - 1;
+          const row = Math.ceil(updated / numberOfColumns);
+          const wrapAround = from % numberOfColumns === 0;
+          if (wrapAround && !useHip) {
+            return false;
+          }
+          return wrapAround
+            ? row * numberOfColumns + numberOfColumns - 1
+            : updated;
         }
-    };
+        case directions.up: {
+          const updated = from - numberOfColumns;
+          const wrapAround = updated < 0;
+          if (wrapAround && !useHip) return false;
+          return wrapAround
+            ? numberOfRows * numberOfColumns +
+                (from % numberOfColumns) -
+                numberOfColumns
+            : updated;
+        }
+        case directions.down: {
+          const updated = from + numberOfColumns;
+          const wrapAround =
+            Math.ceil(updated / numberOfColumns) > numberOfRows ||
+            updated >= numberOfColumns * numberOfRows;
+          if (wrapAround && !useHip) return false;
+          return wrapAround ? from % numberOfColumns : updated;
+        }
+        default: {
+          let updated = from + 1;
+          const wrapAround = updated % numberOfColumns === 0;
+          if (wrapAround && !useHip) return false;
+          return wrapAround ? updated - numberOfColumns : updated;
+        }
+      }
+    },
+  };
 };
 
 let gameState = {
-    numberOfColumns: 0,
-    numberOfRows: 0,
-    get numberOfCells() {
-        return this.numberOfColumns * this.numberOfRows;
-    },
-    level: 1,
-    direction: directions.right,
-    hearts: [],
-    stops: [],
-    score: 0,
-    snake: [],
-    tailLength: 0,
-    levelSpeeds: [Infinity, 500, 450, 400, 350, 300, 250, 200, 150, 100],
-    scoreUp: function () {
-        this.score += this.level * 10;
-    },
-    get head() {
-        return this.snake[0];
-    },
-    get tail() {
-        return this.snake.slice(1, this.tailLength + 1);
-    },
-    get speed() {
-        return (
-            this.levelSpeeds[this.level] ||
-            this.levelSpeeds[this.levelSpeeds.length - 1]
-        );
-    },
-    gameOver: false
+  numberOfColumns: 0,
+  numberOfRows: 0,
+  get numberOfCells() {
+    return this.numberOfColumns * this.numberOfRows;
+  },
+  level: 1,
+  direction: directions.right,
+  hearts: [],
+  stops: [],
+  score: 0,
+  snake: [],
+  tailLength: 0,
+  levelSpeeds: [Infinity, 500, 450, 400, 350, 300, 250, 200, 150, 100],
+  scoreUp: function () {
+    this.score += this.level * 10;
+  },
+  get head() {
+    return this.snake[0];
+  },
+  get tail() {
+    return this.snake.slice(1, this.tailLength + 1);
+  },
+  get speed() {
+    return (
+      this.levelSpeeds[this.level] ||
+      this.levelSpeeds[this.levelSpeeds.length - 1]
+    );
+  },
+  gameOver: false,
 };
 
 const ensureFree = (min, max, alreadyTaken = []) => {
-    const rnd = Math.floor(Math.random() * max);
-    return rnd >= min && !alreadyTaken.includes(rnd)
-        ? rnd
-        : ensureFree(min, max, alreadyTaken);
+  const rnd = Math.floor(Math.random() * max);
+  return rnd >= min && !alreadyTaken.includes(rnd)
+    ? rnd
+    : ensureFree(min, max, alreadyTaken);
 };
 
 const game = snake => {
-    grid.classList.remove("level-clear");
-    gameState.hearts = [];
-    gameState.stops = [];
-    const cells = grid.querySelectorAll(".cell");
-    cells.forEach(cell => {
-        cell.classList.remove("heart");
-        cell.classList.remove("stop");
+  grid.classList.remove("level-clear");
+  gameState.hearts = [];
+  gameState.stops = [];
+  const cells = grid.querySelectorAll(".cell");
+  cells.forEach(cell => {
+    cell.classList.remove("heart");
+    cell.classList.remove("stop");
+  });
+
+  new Array(useHip ? gameState.level + 1 : 1)
+    .fill(heartCssClass)
+    .forEach(cssClass => {
+      const heart = ensureFree(0, gameState.numberOfCells, [
+        gameState.head,
+        ...gameState.tail,
+        gameState.hearts,
+      ]);
+      gameState.hearts.push(heart);
+      cells[heart].classList.add(cssClass);
     });
 
-    new Array(useHip ? gameState.level + 1 : 1)
-        .fill(heartCssClass)
-        .forEach(cssClass => {
-            const heart = ensureFree(0, gameState.numberOfCells, [
-                gameState.head,
-                ...gameState.tail,
-                gameState.hearts
-            ]);
-            gameState.hearts.push(heart);
-            cells[heart].classList.add(cssClass);
-        });
+  if (useHip)
+    new Array(gameState.level + 1).fill("stop").forEach(_ => {
+      const stop = ensureFree(0, gameState.numberOfCells, [
+        gameState.head,
+        ...gameState.tail,
+        ...gameState.hearts,
+        ...gameState.stops,
+      ]);
+      gameState.stops.push(stop);
+      cells[stop].classList.add("stop");
+    });
 
-    if (useHip)
-        new Array(gameState.level + 1).fill("stop").forEach(_ => {
-            const stop = ensureFree(0, gameState.numberOfCells, [
-                gameState.head,
-                ...gameState.tail,
-                ...gameState.hearts,
-                ...gameState.stops
-            ]);
-            gameState.stops.push(stop);
-            cells[stop].classList.add("stop");
-        });
+  if (gameState.level === 1) {
+    gameState.snake.push(
+      ensureFree(1, gameState.numberOfCells, [
+        ...gameState.hearts,
+        ...gameState.stops,
+      ])
+    );
 
-    if (gameState.level === 1) {
-        gameState.snake.push(
-            ensureFree(1, gameState.numberOfCells, [
-                ...gameState.hearts,
-                ...gameState.stops
-            ])
+    gameState.snake.push(gameState.head - 1);
+  }
+
+  let lastTick = 0;
+  gameloop = requestAnimationFrame(function render(timestamp) {
+    if (timestamp - lastTick > gameState.speed) {
+      lastTick = timestamp;
+      cells[gameState.head].classList.remove(
+        snakeHeadCssClass,
+        "right",
+        "up",
+        "down",
+        "left"
+      );
+      cells[gameState.tail[gameState.tail.length - 1]].classList.remove(
+        snakeTailCssClass
+      );
+
+      const next = snake.move(gameState.head, gameState.direction);
+      if (next !== false) gameState.snake.unshift(next);
+      else {
+        gameState.gameOver = true;
+      }
+
+      if (
+        gameState.stops.includes(gameState.head) ||
+        gameState.tail.includes(gameState.head)
+      ) {
+        gameState.gameOver = true;
+      } else if (gameState.hearts.includes(gameState.head)) {
+        cells[gameState.head].classList.remove(heartCssClass);
+        gameState.hearts = gameState.hearts.filter(
+          heart => heart !== gameState.head
         );
+        gameState.tailLength++;
+        gameState.scoreUp();
+        score.textContent = gameState.score;
+      }
 
-        gameState.snake.push(gameState.head - 1);
+      const directionCssClass =
+        gameState.direction === directions.right
+          ? "right"
+          : gameState.direction === directions.up
+          ? "up"
+          : gameState.direction === directions.down
+          ? "down"
+          : "left";
+
+      cells[gameState.head].classList.add(snakeHeadCssClass, directionCssClass);
+      cells[gameState.tail[0]].classList.add(snakeTailCssClass);
     }
 
-    let lastTick = 0;
-    gameloop = requestAnimationFrame(function render(timestamp) {
-        if (timestamp - lastTick > gameState.speed) {
-            lastTick = timestamp;
-            cells[gameState.head].classList.remove(
-                snakeHeadCssClass,
-                "right",
-                "up",
-                "down",
-                "left"
-            );
-            cells[gameState.tail[gameState.tail.length - 1]].classList.remove(
-                snakeTailCssClass
-            );
-
-            const next = snake.move(gameState.head, gameState.direction);
-            if (next !== false) gameState.snake.unshift(next);
-            else {
-                gameState.gameOver = true;
-            }
-
-            if (
-                gameState.stops.includes(gameState.head) ||
-                gameState.tail.includes(gameState.head)
-            ) {
-                gameState.gameOver = true;
-            } else if (gameState.hearts.includes(gameState.head)) {
-                cells[gameState.head].classList.remove(heartCssClass);
-                gameState.hearts = gameState.hearts.filter(
-                    heart => heart !== gameState.head
-                );
-                gameState.tailLength++;
-                gameState.scoreUp();
-                score.textContent = gameState.score;
-            }
-
-            const directionCssClass =
-                gameState.direction === directions.right
-                    ? "right"
-                    : gameState.direction === directions.up
-                    ? "up"
-                    : gameState.direction === directions.down
-                    ? "down"
-                    : "left";
-
-            cells[gameState.head].classList.add(
-                snakeHeadCssClass,
-                directionCssClass
-            );
-            cells[gameState.tail[0]].classList.add(snakeTailCssClass);
-        }
-
-        if (gameState.gameOver) {
-            grid.classList.add("game-over");
-        } else if (!gameState.hearts.length) {
-            grid.classList.add("level-clear");
-            gameState.level++;
-            level.textContent = gameState.level;
-            game(snake);
-        } else {
-            gameloop = requestAnimationFrame(render);
-        }
-    });
+    if (gameState.gameOver) {
+      grid.classList.add("game-over");
+    } else if (!gameState.hearts.length) {
+      grid.classList.add("level-clear");
+      gameState.level++;
+      level.textContent = gameState.level;
+      game(snake);
+    } else {
+      gameloop = requestAnimationFrame(render);
+    }
+  });
 };
 
 start.addEventListener("click", () => {
-    document.activeElement.blur();
-    useHip =
-        document.querySelector('input[name="game-style"]:checked').value ===
-        "hip";
-    if (useHip) {
-        grid.classList.add("hip");
-        grid.classList.remove("vintage");
-    } else {
-        grid.classList.remove("hip");
-        grid.classList.add("vintage");
-    }
-    grid.classList.remove("game-over");
-    gameState.gameOver = false;
-    gameState.numberOfColumns = +document.querySelector("#columns").value;
-    gameState.numberOfRows = +document.querySelector("#rows").value;
-    gameState.level = 1;
-    gameState.snake = [];
-    gameState.tailLength = 1;
-    gameState.direction = directions.right;
-    gameState.score = 0;
+  document.activeElement.blur();
+  useHip =
+    document.querySelector('input[name="game-style"]:checked').value === "hip";
+  if (useHip) {
+    grid.classList.add("hip");
+    grid.classList.remove("vintage");
+  } else {
+    grid.classList.remove("hip");
+    grid.classList.add("vintage");
+  }
+  grid.classList.remove("game-over");
+  gameState.gameOver = false;
+  gameState.numberOfColumns = +document.querySelector("#columns").value;
+  gameState.numberOfRows = +document.querySelector("#rows").value;
+  gameState.level = 1;
+  gameState.snake = [];
+  gameState.tailLength = 1;
+  gameState.direction = directions.right;
+  gameState.score = 0;
 
-    cancelAnimationFrame(gameloop);
+  cancelAnimationFrame(gameloop);
 
-    const cells = new Array(gameState.numberOfCells)
-        .fill(document.createElement("span"))
-        .map(span => {
-            const cell = span.cloneNode();
-            cell.classList.add("cell");
-            return cell;
-        });
-
-    requestAnimationFrame(() => {
-        level.textContent = gameState.level;
-        score.textContent = gameState.score;
-
-        grid.innerHTML = "";
-        grid.style.gridTemplateColumns = `repeat(${gameState.numberOfColumns}, 1fr)`;
-        cells.forEach(cell => {
-            grid.appendChild(cell);
-        });
-
-        const snake = Snake(gameState.numberOfColumns, gameState.numberOfRows);
-
-        game(snake);
+  const cells = new Array(gameState.numberOfCells)
+    .fill(document.createElement("span"))
+    .map(span => {
+      const cell = span.cloneNode();
+      cell.classList.add("cell");
+      return cell;
     });
+
+  requestAnimationFrame(() => {
+    level.textContent = gameState.level;
+    score.textContent = gameState.score;
+
+    grid.innerHTML = "";
+    grid.style.gridTemplateColumns = `repeat(${gameState.numberOfColumns}, 1fr)`;
+    cells.forEach(cell => {
+      grid.appendChild(cell);
+    });
+
+    const snake = Snake(gameState.numberOfColumns, gameState.numberOfRows);
+
+    game(snake);
+  });
 });
 
 const updateDirection = direction => {
-    switch (direction) {
-        case directions.left:
-        case directions.right:
-            if (
-                gameState.direction !== directions.left &&
-                gameState.direction !== directions.right
-            )
-                gameState.direction = direction;
-            break;
-        case directions.up:
-        case directions.down:
-            if (
-                gameState.direction !== directions.up &&
-                gameState.direction !== directions.down
-            )
-                gameState.direction = direction;
-            break;
-    }
+  switch (direction) {
+    case directions.left:
+    case directions.right:
+      if (
+        gameState.direction !== directions.left &&
+        gameState.direction !== directions.right
+      )
+        gameState.direction = direction;
+      break;
+    case directions.up:
+    case directions.down:
+      if (
+        gameState.direction !== directions.up &&
+        gameState.direction !== directions.down
+      )
+        gameState.direction = direction;
+      break;
+  }
 };
 
 window.addEventListener("keydown", event => {
-    switch (event.key) {
-        case directions.left:
-            updateDirection(directions.left);
-            break;
-        case directions.up:
-            updateDirection(directions.up);
-            break;
-        case directions.right:
-            updateDirection(directions.right);
-            break;
-        case directions.down:
-            updateDirection(directions.down);
-            break;
-    }
+  switch (event.key) {
+    case directions.left:
+      updateDirection(directions.left);
+      break;
+    case directions.up:
+      updateDirection(directions.up);
+      break;
+    case directions.right:
+      updateDirection(directions.right);
+      break;
+    case directions.down:
+      updateDirection(directions.down);
+      break;
+  }
 });
 
 document.querySelector(".left").addEventListener("touchstart", event => {
-    updateDirection(directions.left);
+  updateDirection(directions.left);
 });
 
 document.querySelector(".up").addEventListener("touchstart", event => {
-    updateDirection(directions.up);
+  updateDirection(directions.up);
 });
 
 document.querySelector(".down").addEventListener("touchstart", event => {
-    updateDirection(directions.down);
+  updateDirection(directions.down);
 });
 
 document.querySelector(".right").addEventListener("touchstart", event => {
-    updateDirection(directions.right);
+  updateDirection(directions.right);
 });
